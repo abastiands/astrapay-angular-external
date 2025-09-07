b94c0dc871b3da4e97fd5174f4d287a1a01513c9
Abastian Dwi Saputra <abastiands@gmail.com>
2025-09-07 13:05:20 +0700

    simplenoteapp: make layout navbar, main, footer

--- src/app/layouts/layout.ts
+++ src/app/layouts/layout.ts
@@ -2,11 +2,12 @@ import { Component } from "@angular/core";
 import { SidebarComponent } from "./sidebar/sidebar";
 import { MainComponent } from "./main/main";
 import { NavbarComponent } from "./navbar/navbar";
+import { FooterComponent } from "./footer/footer";
 
 @Component({
     selector: 'app-layout',
     standalone: true,
-    imports: [SidebarComponent, NavbarComponent, MainComponent],
+    imports: [SidebarComponent, NavbarComponent, MainComponent, FooterComponent],
     templateUrl: './layout.html'
 })
 export class LayoutComponent {}
