5995bdfbdbe69e17a762c37855bf9339ae4b66bf
Abastian Dwi Saputra <abastiands@gmail.com>
2025-09-07 14:41:51 +0700

    simplenoteapp: make total card component

---
+++ src/app/components/totalcard/total-card.ts
@@ -0,0 +1,13 @@
+import { Component, input, signal } from "@angular/core";
+
+@Component({
+    selector: 'app-total-card',
+    standalone: true,
+    templateUrl: './total-card.html'
+})
+export class TotalCardComponent {
+    title = input<string>('Total');
+    value = input<number>(0);
+    changeColor = input<string>('secondary');
+    compareValue = input<string>('+0');
+}
